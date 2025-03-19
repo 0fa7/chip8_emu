b14f8751a930e82c771d2e0c21beab57b3e23f98
0fa7 <7063015+0fa7@users.noreply.github.com>
2025-03-18 23:30:23 -0400

    More instrs to fns

--- src/disassembler.c
+++ src/disassembler.c
@@ -33,10 +33,15 @@ void instr_8xy7(Chip8Cpu* cpu, uint8_t x, uint8_t y);
 void instr_8xye(Chip8Cpu* cpu, uint8_t x, uint8_t y);
 void instr_9xy0(Chip8Cpu* cpu, uint8_t x, uint8_t y);
 void instr_annn(Chip8Cpu* cpu, uint16_t nnn);
+void instr_bnnn(Chip8Cpu* cpu, uint16_t nnn);
+void instr_cxnn(Chip8Cpu* cpu, uint8_t x, uint8_t nn);
 void instr_dxyn(Chip8Cpu* cpu, uint8_t x, uint8_t y, uint8_t n);
+void instr_ex9e(Chip8Cpu* cpu, uint8_t x);
+void instr_exa1(Chip8Cpu* cpu, uint8_t x);
 void instr_fx07(Chip8Cpu* cpu, uint8_t x);
 void instr_fx0a(Chip8Cpu* cpu, uint8_t x);
 void instr_fx15(Chip8Cpu* cpu, uint8_t x);
+void instr_unrecognized(uint8_t h, uint8_t l);
 
 int main(int argc, char** argv)
 {
@@ -142,8 +147,7 @@ void parse_instructions(uint8_t* buffer, long buffer_sz)
 
                 if(ll != 0)
                 {
-                    printf("Unrecognized instruction 0xE\n");
-                    exit(EXIT_SUCCESS);
+                    instr_unrecognized(buffer[i], buffer[i + 1]);
                     break;
                 }
                 
@@ -214,19 +218,66 @@ void parse_instructions(uint8_t* buffer, long buffer_sz)
                         instr_8xy7(cpu, x, y);
                         break;
                     }
-                    case e:
+                    case 0xE:
                     {
                         instr_8xye(cpu, x, y);
                         break;
                     }
                     default:
                     {
-                        printf("Unrecognized instruction 0x%02X%02x\n", buffer[i], buffer[i + 1]);
-                        exit(1);
+                        instr_unrecognized(buffer[i], buffer[i + 1]);
                         break;
                     }
                 }
 
+                break;
+            }
+            case 0x9:
+            {
+                uint8_t ll = buffer[i + 1] & 0x0F;
+
+                switch(ll)
+                {
+                    case 0:
+                    {
+                        uint8_t x = buffer[i] & 0x0F;
+                        uint8_t y = buffer[i + 1] >> 4;
+                        instr_9xy0(cpu, x, y);
+                        break;
+                    }
+                    default:
+                    {
+                        instr_unrecognized(buffer[i], buffer[i + 1]);
+                        break;
+                    }
+                }
+                break;
+            }
+            case 0xA:
+            {
+                uint16_t nnn = (0x0F & buffer[i]) << 8 | buffer[i + 1];
+                instr_annn(cpu, nnn);
+                break;
+            }
+            case 0xB:
+            {
+                uint16_t nnn = (0x0F & buffer[i]) << 8 | buffer[i + 1];
+                instr_bnnn(cpu, nnn);
+                break;
+            }
+            case 0xC:
+            {
+                uint8_t nn = buffer[i + 1];
+                uint8_t x = buffer[i] & 0x0F;
+                instr_cxnn(cpu, x, nn);
+                break;
+            }
+            case 0xD:
+            {
+                uint8_t hl = h & 0x0F;
+                uint8_t y = buffer[i + 1] >> 4;
+                uint8_t n = buffer[i + 1] && 0x0F;
+                instr_dxyn(cpu, hl, y, n);
                 break;
             }
             case 0xE:
@@ -238,38 +289,21 @@ void parse_instructions(uint8_t* buffer, long buffer_sz)
                 {
                     case 0x9E:
                     {
-                        printf("0xE%01X9E ", hl);
-                        printf("Skip the following instruction if the key corresponding to the hex value currently stored in register v[%01X] is pressed\n", hl);
+                        instr_ex9e(cpu, hl);
                         break;
                     }
                     case 0xA1:
                     {
-                        printf("0xE%01XA1 ", hl);
-                        printf("Skip the following instruction if the key corresponding to the hex value currently stored in register v[%01X] is not pressed\n", hl);
+                        instr_exa1(cpu, hl);
                         break;
                     }
                     default:
                     {
-                        printf("Unrecognized instruction 0xE\n");
-                        exit(EXIT_SUCCESS);
+                        instr_unrecognized(buffer[i], buffer[i + 1]);
                         break;
                     }
                 }
 
-                break;
-            }
-            case 0xA:
-            {
-                uint16_t nnn = (0x0F & buffer[i]) << 8 | buffer[i + 1];
-                instr_annn(cpu, nnn);
-                break;
-            }
-            case 0xD:
-            {
-                uint8_t hl = h & 0x0F;
-                uint8_t y = buffer[i + 1] >> 4;
-                uint8_t n = buffer[i + 1] && 0x0F;
-                instr_dxyn(cpu, hl, y, n);
                 break;
             }
             case 0xF:
@@ -305,8 +339,7 @@ void parse_instructions(uint8_t* buffer, long buffer_sz)
                     }
                     default:
                     {
-                        printf("Unrecognized instruction 0xF\n");
-                        exit(EXIT_SUCCESS);
+                        instr_unrecognized(buffer[i], buffer[i + 1]);
                         break;
                     }
                 }
@@ -315,8 +348,7 @@ void parse_instructions(uint8_t* buffer, long buffer_sz)
             }
             default:
             {
-                printf("Unrecognized instruction\n");
-                exit(EXIT_SUCCESS);
+                instr_unrecognized(buffer[i], buffer[i + 1]);
                 break;
             }
         };
@@ -464,7 +496,8 @@ void instr_8xye(Chip8Cpu* cpu, uint8_t x, uint8_t y)
 // 0x9XY0
 void instr_9xy0(Chip8Cpu* cpu, uint8_t x, uint8_t y)
 {
-
+    printf("0x9%01X%01X0 ", x, y);
+    printf("Skip the following instruction if the value of register v[%01X] is not equal to the value of register v[%01X]\n", x, y);
 }
 
 // 0xANNN
@@ -474,11 +507,39 @@ void instr_annn(Chip8Cpu* cpu, uint16_t nnn)
     printf("Load the value 0x%03X in register I\n", nnn);
 }
 
+// 0xBNNN
+void instr_bnnn(Chip8Cpu* cpu, uint16_t nnn)
+{
+    printf("0xB%03X ", nnn);
+    printf("Jump to address 0x%03X + v[0x0]\n", nnn);
+}
+
+// 0xCXNN
+void instr_cxnn(Chip8Cpu* cpu, uint8_t x, uint8_t nn)
+{
+    printf("0xC%01X%02X ", x, nn);
+    printf("Set v[0x%01X] to a random number with a mask of 0x%02X", x, nn);
+}
+
 // 0xDXYN
 void instr_dxyn(Chip8Cpu* cpu, uint8_t x, uint8_t y, uint8_t n)
 {
     printf("0xD%01X%01X%01X ", x, y, n);
     printf("Draw a %01X byte sprite starting at memory location stored in register I at v[%01X] v[%01X]\n", x , y, n);
+}
+
+// 0xEX9E
+void instr_ex9e(Chip8Cpu* cpu, uint8_t x)
+{
+    printf("0xE%01X9E ", x);
+    printf("Skip the following instruction if the key corresponding to the hex value currently stored in register v[%01X] is pressed\n", x);
+}
+
+// 0xEXA1
+void instr_exa1(Chip8Cpu* cpu, uint8_t x)
+{
+    printf("0xE%01XA1 ", x);
+    printf("Skip the following instruction if the key corresponding to the hex value currently stored in register v[%01X] is not pressed\n", x);
 }
 
 // 0xFX07
@@ -500,4 +561,12 @@ void instr_fx15(Chip8Cpu* cpu, uint8_t x)
 {
     printf("0xF%x15 ", x);
     printf("Set the delay timer to the value stored in register v[%01X]\n", x);
+}
+
+// Unrecognized
+void instr_unrecognized(uint8_t h, uint8_t l)
+{
+    printf("0x%02X%02X ", h, l);
+    printf("Unrecognized instruction.");
+    exit(EXIT_SUCCESS);
 }
